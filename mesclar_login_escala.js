dcf23df07054f0bcb9220e978b988d52c44b8dcc
LiderBpo <eliabe.liderbpo@gmail.com>
2026-09-13 22:26:19 -0300

    Delete mesclar_login_escala.js

--- mesclar_login_escala.js
+++
@@ -1,6 +0,0 @@
-function mesclarLoginEscala(nova, antiga){
- return nova.map(item=>{
-  let old=antiga.find(u=> (u.nome||"").toLowerCase()===(item.nome||"").toLowerCase());
-  return {...item, usuario:old?.usuario||item.usuario, senha:old?.senha||"ANS#2026"};
- });
-}
